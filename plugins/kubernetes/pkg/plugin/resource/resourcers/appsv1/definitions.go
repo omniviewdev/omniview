b7189c4026cab1f0bbd69492f639a3e09304cc8a
Joshua Pare <joshuabpare@gmail.com>
2024-04-10 19:09:36 -0500

    fix live connection resync

--- plugins/kubernetes/pkg/plugin/resource/resourcers/appsv1/definitions.go
+++ plugins/kubernetes/pkg/plugin/resource/resourcers/appsv1/definitions.go
@@ -5,11 +5,53 @@ import "github.com/omniviewdev/plugin-sdk/pkg/resource/types"
 
 var DeploymentCols = []types.ColumnDef{
 	{
+		ID:        "availableReplicas",
+		Header:    "Available",
+		Accessors: "status.availableReplicas",
+		Alignment: types.CellAlignmentLeft,
+		Width:     100,
+	},
+	{
 		ID:        "replicas",
 		Header:    "Replicas",
 		Accessors: "status.replicas",
-		Alignment: types.CellAlignmentCenter,
+		Alignment: types.CellAlignmentLeft,
 		Width:     80,
+	},
+	{
+		ID:        "readyReplicas",
+		Header:    "Ready",
+		Accessors: "status.readyReplicas",
+		Alignment: types.CellAlignmentLeft,
+		Width:     100,
+	},
+	{
+		ID:        "updatedReplicas",
+		Header:    "Updated",
+		Accessors: "status.updatedReplicas",
+		Alignment: types.CellAlignmentLeft,
+		Width:     100,
+	},
+	{
+		ID:        "revisionHistoryLimit",
+		Header:    "Revision History Limit",
+		Accessors: "spec.revisionHistoryLimit",
+		Alignment: types.CellAlignmentLeft,
+		Width:     120,
+	},
+	{
+		ID:        "strategy",
+		Header:    "Strategy",
+		Accessors: "spec.strategy.type",
+		Alignment: types.CellAlignmentLeft,
+		Width:     100,
+	},
+	{
+		ID:        "minReadySeconds",
+		Header:    "Min Ready Seconds",
+		Accessors: "spec.minReadySeconds",
+		Alignment: types.CellAlignmentLeft,
+		Width:     100,
 	},
 }
 
